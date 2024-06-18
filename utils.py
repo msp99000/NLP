d04554a47c3825b97497ab4a9df27c4eab173e85
Manpreet Singh <msp99000@gmail.com>
2024-06-19 00:19:17 +0530

    feat: adds root path and data path

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 
-print(__file__)
-print(os.path.dirname(__file__))
-print(os.path.abspath(__file__))
+# Root folder of the project
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Data folder for the project
+data_dir = os.path.join(root_dir, "data")  # Move up one directory
